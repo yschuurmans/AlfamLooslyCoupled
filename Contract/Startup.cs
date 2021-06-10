17adb886b914b24a28caa615565592307e81c043
Youri Schuurmans <Youri.Schuurmans@InfoSupport.com>
2021-06-10 13:16:37 +0200

    publishing works

--- Contract/Startup.cs
+++ Contract/Startup.cs
@@ -26,6 +26,8 @@ namespace Contract
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<RabbitMQPublisher>();
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
